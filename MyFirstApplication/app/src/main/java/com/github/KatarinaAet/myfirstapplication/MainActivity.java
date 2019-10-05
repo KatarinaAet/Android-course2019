694dc6adc492758026074822fc29947bae3a2ab3
KatarinaAet <Katarina.Kulagina97@yandex.ru>
2019-10-05 22:47:55 +0300

    add button send

--- MyFirstApplication/app/src/main/java/com/github/KatarinaAet/myfirstapplication/MainActivity.java
+++ MyFirstApplication/app/src/main/java/com/github/KatarinaAet/myfirstapplication/MainActivity.java
@@ -1,14 +1,31 @@
 package com.github.KatarinaAet.myfirstapplication;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+import static com.github.KatarinaAet.myfirstapplication.R.layout.activity_note1;
 
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        final Button buttonSend = findViewById(R.id.email);
+        buttonSend.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        Intent emailIntent = new Intent(Intent.ACTION_SENDTO, Uri.fromParts(
+                "mailto","android-dev@yandex.ru", null));
+        emailIntent.putExtra(Intent.EXTRA_SUBJECT, "Notes feedback");
+        startActivity(Intent.createChooser(emailIntent, "Sending"));
     }
 }
---
+++ MyFirstApplication/app/src/main/java/com/github/KatarinaAet/myfirstapplication/Note1.java
@@ -0,0 +1,12 @@
+package com.github.KatarinaAet.myfirstapplication;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+
+public class Note1 extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
